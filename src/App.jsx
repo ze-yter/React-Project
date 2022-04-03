7cb5a3755466e47366f5a1ac5883e6907487cb33
KPEesti <iwantoseeyou1@gmail.com>
2022-04-03 16:32:30 +0500

    Добавил API для закладок, реализовал добавление

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import CardItem from "./components/CardItem";
 import Header from "./components/HeaderItem";
-import Drawer from "./components/CartDrawer";
+import Drawer from "./components/Drawer";
 import { useEffect, useState } from "react";
 import SearchBlock from "./components/SearchBlock";
 import axios from 'axios';
@@ -9,6 +9,7 @@ function App() {
 
   const [items, setItems] = useState([]);
   const [cartItems, setCartItems] = useState([]);
+  const [favorites, setFavorites] = useState([]);
   const [searchValue, setSearchValue] = useState('');
   const [cartOpened, setCartOpened] = useState(false);
 
@@ -28,11 +29,16 @@ function App() {
     axios.post('https://62460a29e3450d61b0fa28c9.mockapi.io/cart', item);
     setCartItems(prev => [...prev, item]);
   };
-  
+
   const onDeleteCartItem = (id) => {
     axios.delete(`https://62460a29e3450d61b0fa28c9.mockapi.io/cart/${id}`);
     setCartItems(prev => prev.filter(item => item.id !== id));
   }
+
+  const onAddToFavorite = (item) => {
+    axios.post('https://62460a29e3450d61b0fa28c9.mockapi.io/favorites', item);
+    setFavorites(prev => [...prev, item]);
+  };
 
   return (
     <div className="wrapper">
@@ -52,13 +58,14 @@ function App() {
             .map(item => {
               return <CardItem
                 key={item.id}
-                name = {item.name}
+                name={item.name}
                 price={item.price}
                 imgSrc={item.imgSrc}
-                onFavorite={() => console.log('Добавили в закладки')}
+                onFavorite={obj => onAddToFavorite(obj)}
                 onAddCart={obj => onAddToCart(obj)}
               />
-            })}
+            })
+          }
         </div>
       </div>
     </div>
--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,17 +1,23 @@
 import React, { useState } from 'react'
 
-function CardItem({name, price, imgSrc,  onAddCart, onFavorite}) {
-const [isAdded, setIsAdded] = useState(false);
+function CardItem({ name, price, imgSrc, onAddCart, onFavorite }) {
+    const [isAdded, setIsAdded] = useState(false);
+    const [isFavorite, setIsFavorite] = useState(false);
 
-const onClickPlus = () => {
-    onAddCart({name, price, imgSrc});
-    setIsAdded(!isAdded);
-};
+    const onClickPlus = () => {
+        onAddCart({ name, price, imgSrc });
+        setIsAdded(!isAdded);
+    };
+
+    const onClickFavorite = () => {
+        onFavorite({ name, price, imgSrc });
+        setIsFavorite(!isFavorite);
+    };
 
     return (
         <div className="card">
-            <div className='favorite'>
-                <img width={32} height={32} className='' src="images/unliked.svg" alt="Favorite" />
+            <div className='favorite' onClick={onClickFavorite}>
+                <img width={32} height={32} className='' src={isFavorite ? "images/liked.svg" : "images/unliked.svg"} alt="Favorite" />
             </div>
             <div className='card__img'>
                 <img width={133} height={112} src={imgSrc} alt="" />
@@ -22,11 +28,11 @@ const onClickPlus = () => {
                     <span>Цена:</span>
                     <b>{price} руб.</b>
                 </div>
-                <img 
-                className='button' 
-                width={32} height={32} 
-                onClick={onClickPlus}
-                src={isAdded ? "/images/addButton-checked.svg" : "/images/addButton-unchecked.svg"} alt="AddButton" />
+                <img
+                    className='button'
+                    width={32} height={32}
+                    onClick={onClickPlus}
+                    src={isAdded ? "/images/addButton-checked.svg" : "/images/addButton-unchecked.svg"} alt="AddButton" />
             </div>
         </div>
     );
